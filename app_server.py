31e2123d11f1f25c2676679f05a9ae0fd0588eae
mkalinowski11 <michal.kalinowski321@gmail.com>
2024-04-22 00:15:00 +0200

    Initial commit of chatbot example

---
+++ app_server.py
@@ -0,0 +1,51 @@
+# import transformers
+from flask import Flask
+from flask import request
+import time
+from llama_cpp import Llama
+
+SERVER_PORT = 9501
+HOST_URL="127.0.0.1"
+MODEL_PATH = "./models/codellama-7b.Q3_K_L.gguf"
+# MODEL_PATH = "./models/vicuna-13b-v1.5.gguf"
+
+
+
+
+def get_model():
+    print("="*100, "\nModel Info\n", "="*100, "\n")
+    model = Llama(
+        model_path=MODEL_PATH,
+        n_ctx=512,
+        n_batch=48
+    )
+    print("\n", "="*100, "\nModel Info\n", "="*100)
+    return model
+
+model = get_model()
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<p>Test server!</p>"
+
+def generate_response(prompt, max_tokens=512, temperature=0.2, top_p=0.5, stop=["#"], **kwargs):
+    response = model(
+        prompt=prompt,
+        max_tokens=max_tokens,
+        temperature=temperature,
+        top_p=top_p,
+        stop=stop,
+        **kwargs
+    )
+    return response
+
+@app.route("/predict", methods=["POST"])
+def predict_response():
+    if request.method == "POST":
+        data = request.get_json()
+        response = generate_response(**data)
+        return response
+
+if __name__ == "__main__":
+    app.run(host=HOST_URL, port=SERVER_PORT)
---
+++ llm_server_test.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+SERVER_PORT = 9501
+HOST_URL="127.0.0.1"
+URL = f"http://{HOST_URL}:{SERVER_PORT}/predict"
+
+def format_prompt(prompt):
+    return f"""<|im_start|>system
+You are a helpful chatbot.<|im_end|>
+<|im_start|>user
+{prompt}<|im_end|>"""
+
+def send_request(prompt):
+    # prompt=format_prompt(prompt)
+    data={
+        "prompt": prompt,
+        "max_tokens": 128,
+        "temperature": 0.4,
+        "top_p": 0.5
+    }
+    return requests.post(URL, json=data)
+
+if __name__ == "__main__":
+    prompt = "Why the earth is movind around the sun"
+    print("provided prompt ::", prompt)
+    response = send_request(prompt)
+    response = response.json()
+    print("got response ::")
+    print(response)
